ae287f3fda18528e665d76d6f22067b29179baef
Christoph Hochstrasser <christoph@hochstrasser.io>
2023-09-05 17:27:12 +0200

    OOE/Laufend: Tue Sep  5 15:27:12 UTC 2023

--- scripts/ooe-laufend.sh
+++ scripts/ooe-laufend.sh
@@ -1,8 +1,8 @@
-curl --etag-save ../downloads/ooe_laufend.etag \
-    --etag-compare ../downloads/ooe_laufend.etag \
-    https://cf-intranet.ooelfv.at/webext2/rss/json_laufend.txt -o ../downloads/ooe_laufend.json
+curl --etag-save ./downloads/ooe_laufend.etag \
+    --etag-compare ./downloads/ooe_laufend.etag \
+    https://cf-intranet.ooelfv.at/webext2/rss/json_laufend.txt -o ./downloads/ooe_laufend.json
 
-jq --from-file ../downloads/ooe_laufend.json \
+jq --from-file ./downloads/ooe_laufend.json \
     --sort-keys '.einsaetze = (.einsaetze//{} | to_entries | map(.value) | sort_by(.value.einsatz.startzeit)) | del(.pubDate)' \
     > ./data/json_laufend.json
 
