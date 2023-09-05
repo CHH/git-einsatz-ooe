15b6e636d3e10749166445d6d95135a10ca9936d
Christoph Hochstrasser <christoph@hochstrasser.io>
2023-09-05 17:45:01 +0200

    Run jq only when file exists

--- scripts/ooe-laufend.sh
+++ scripts/ooe-laufend.sh
@@ -2,5 +2,5 @@ curl --fail --etag-save ./downloads/ooe_laufend.etag \
     --etag-compare ./downloads/ooe_laufend.etag \
     https://cf-intranet.ooelfv.at/webext2/rss/json_laufend.txt -o ./downloads/ooe_laufend.json
 
-cat ./downloads/ooe_laufend.json | jq --sort-keys '.einsaetze = (.einsaetze//{} | to_entries | map(.value) | sort_by(.value.einsatz.startzeit)) | del(.pubDate)' \
+test -f ./downloads/ooe_laufend.etag && cat ./downloads/ooe_laufend.json | jq --sort-keys '.einsaetze = (.einsaetze//{} | to_entries | map(.value) | sort_by(.value.einsatz.startzeit)) | del(.pubDate)' \
     > ./data/json_laufend.json
