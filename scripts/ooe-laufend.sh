feb2e998ee9cc5697061586b36ac7bb37bc002d5
Christoph Hochstrasser <christoph@hochstrasser.io>
2024-02-16 11:23:26 +0100

    Change to new URL

--- scripts/ooe-laufend.sh
+++ scripts/ooe-laufend.sh
@@ -1,6 +1,6 @@
 curl --fail --etag-save ./downloads/ooe_laufend.etag \
     --etag-compare ./downloads/ooe_laufend.etag \
-    https://cf-intranet.ooelfv.at/webext2/rss/json_laufend.txt -o ./downloads/ooe_laufend.json
+    https://cf-einsaetze.ooelfv.at/webext2/rss/json_laufend.txt -o ./downloads/ooe_laufend.json
 
 test -f ./downloads/ooe_laufend.etag && cat ./downloads/ooe_laufend.json | jq --sort-keys '.einsaetze = (.einsaetze//{} | to_entries | map(.value) | sort_by(.value.einsatz.startzeit)) | del(.pubDate)' \
     > ./data/json_laufend.json
